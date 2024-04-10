7c8ec597f6da955a7bb698c0d05aac520030b4ca
Joseph Martin <124799388+vinhnasa@users.noreply.github.com>
2024-04-10 16:19:57 +0700

    implement clerk webhooks

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,126 +8,126 @@ declare type CreateUserParams = {
     firstName: string;
     lastName: string;
     photo: string;
-  };
-  
-  declare type UpdateUserParams = {
+};
+
+declare type UpdateUserParams = {
     firstName: string;
     lastName: string;
     username: string;
     photo: string;
-  };
-  
-  // ====== IMAGE PARAMS
-  declare type AddImageParams = {
+};
+
+// ====== IMAGE PARAMS
+declare type AddImageParams = {
     image: {
-      title: string;
-      publicId: string;
-      transformationType: string;
-      width: number;
-      height: number;
-      config: any;
-      secureURL: string;
-      transformationURL: string;
-      aspectRatio: string | undefined;
-      prompt: string | undefined;
-      color: string | undefined;
+        title: string;
+        publicId: string;
+        transformationType: string;
+        width: number;
+        height: number;
+        config: any;
+        secureURL: string;
+        transformationURL: string;
+        aspectRatio: string | undefined;
+        prompt: string | undefined;
+        color: string | undefined;
     };
     userId: string;
     path: string;
-  };
-  
-  declare type UpdateImageParams = {
+};
+
+declare type UpdateImageParams = {
     image: {
-      _id: string;
-      title: string;
-      publicId: string;
-      transformationType: string;
-      width: number;
-      height: number;
-      config: any;
-      secureURL: string;
-      transformationURL: string;
-      aspectRatio: string | undefined;
-      prompt: string | undefined;
-      color: string | undefined;
+        _id: string;
+        title: string;
+        publicId: string;
+        transformationType: string;
+        width: number;
+        height: number;
+        config: any;
+        secureURL: string;
+        transformationURL: string;
+        aspectRatio: string | undefined;
+        prompt: string | undefined;
+        color: string | undefined;
     };
     userId: string;
     path: string;
-  };
-  
-  declare type Transformations = {
+};
+
+declare type Transformations = {
     restore?: boolean;
     fillBackground?: boolean;
     remove?: {
-      prompt: string;
-      removeShadow?: boolean;
-      multiple?: boolean;
+        prompt: string;
+        removeShadow?: boolean;
+        multiple?: boolean;
     };
     recolor?: {
-      prompt?: string;
-      to: string;
-      multiple?: boolean;
+        prompt?: string;
+        to: string;
+        multiple?: boolean;
     };
     removeBackground?: boolean;
-  };
-  
-  // ====== TRANSACTION PARAMS
-  declare type CheckoutTransactionParams = {
+};
+
+// ====== TRANSACTION PARAMS
+declare type CheckoutTransactionParams = {
     plan: string;
     credits: number;
     amount: number;
     buyerId: string;
-  };
-  
-  declare type CreateTransactionParams = {
+};
+
+declare type CreateTransactionParams = {
     stripeId: string;
     amount: number;
     credits: number;
     plan: string;
     buyerId: string;
     createdAt: Date;
-  };
-  
-  declare type TransformationTypeKey =
+};
+
+declare type TransformationTypeKey =
     | "restore"
     | "fill"
     | "remove"
     | "recolor"
     | "removeBackground";
-  
-  // ====== URL QUERY PARAMS
-  declare type FormUrlQueryParams = {
+
+// ====== URL QUERY PARAMS
+declare type FormUrlQueryParams = {
     searchParams: string;
     key: string;
     value: string | number | null;
-  };
-  
-  declare type UrlQueryParams = {
+};
+
+declare type UrlQueryParams = {
     params: string;
     key: string;
     value: string | null;
-  };
-  
-  declare type RemoveUrlQueryParams = {
+};
+
+declare type RemoveUrlQueryParams = {
     searchParams: string;
     keysToRemove: string[];
-  };
-  
-  declare type SearchParamProps = {
+};
+
+declare type SearchParamProps = {
     params: { id: string; type: TransformationTypeKey };
     searchParams: { [key: string]: string | string[] | undefined };
-  };
-  
-  declare type TransformationFormProps = {
+};
+
+declare type TransformationFormProps = {
     action: "Add" | "Update";
     userId: string;
     type: TransformationTypeKey;
     creditBalance: number;
     data?: IImage | null;
     config?: Transformations | null;
-  };
-  
-  declare type TransformedImageProps = {
+};
+
+declare type TransformedImageProps = {
     image: any;
     type: string;
     title: string;
@@ -135,4 +135,4 @@ declare type CreateUserParams = {
     isTransforming: boolean;
     hasDownload?: boolean;
     setIsTransforming?: React.Dispatch<React.SetStateAction<boolean>>;
-  };
+};
